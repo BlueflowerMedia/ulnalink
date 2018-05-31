43743501b5f636483d7df9ae5166bfb4fa03e22b
Chandra Shankar Shrestha <you@example.com>
2018-05-31 16:25:36 -0400

    banner complete

--- front-page.php
+++ front-page.php
@@ -21,35 +21,57 @@
   </div>
 </section>
 
+<!-- Blank space between header and banner-->
+<section class="blank-space">
+  <div class="blank-space">
 
+  </div>
+</section>
 
-
+<!--banner-->
 <section class="banner">
+
+  <div class="triangle">
+
+  </div>
   <div class="container flex-row">
-    <div class="list-left flex1">
+    <div class="list-left flex">
       <p id="box">1</p>
     </div>
-    <div id="1" class="list-right flex1">
+    <div id="1" class="list-right flex">
       <p>Search around the world with advanced filters.</p>
       <p id="bottom">This is a uesful tool to work with, it will help assist finding better offers.</p>
     </div>
   </div>
 
   <div class="container flex-row">
-    <div class="list-left flex1">
+
+    <div class="list-left flex">
+      <p id="box-white"></p>
+    </div>
+    <div class="list-left flex">
       <p id="box">2</p>
     </div>
-    <div class="list-right flex1">
+
+
+    <div id="2" class="list-right flex">
       <p>Auto share careers through social media.<p>
       <p id="bottom">Selected your profrssion? Great, create connection on this niche website.</p>
     </div>
   </div>
 
   <div class="container flex-row">
-    <div class="list-left flex1">
+    <div class="list-left flex">
+      <p id="box-white"></p>
+    </div>
+    <div class="list-left flex">
+      <p id="box-white"></p>
+    </div>
+
+    <div class="list-left flex">
       <p id="box">3</p>
     </div>
-    <div class="list-right flex1">
+    <div  id="3" class="list-right flex">
       <p>Free!</p>
       <p id="bottom">This site will offer free service to those who need to find work.</p>
     </div>
