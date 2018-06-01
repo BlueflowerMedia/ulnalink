fee8cb3e3471091d5910bf06247e91295f088027
BThapa <bthapa7@senecacollege.ca>
2018-06-01 15:36:58 -0400

    fixed

--- front-page.php
+++ front-page.php
@@ -21,7 +21,6 @@
   </div>
 </section>
 
-
 <!-- Blank space between header and banner-->
 <section class="blank-space">
   <div class="blank-space">
@@ -30,7 +29,6 @@
 </section>
 
 <!--banner-->
-
 <section class="banner">
 
   <div class="triangle">
@@ -41,7 +39,7 @@
       <p id="box">1</p>
     </div>
     <div id="1" class="list-right flex">
-      <p id="top" >Search around the world with advanced filters.</p>
+      <p>Search around the world with advanced filters.</p>
       <p id="bottom">This is a uesful tool to work with, it will help assist finding better offers.</p>
     </div>
   </div>
@@ -57,7 +55,7 @@
 
 
     <div id="2" class="list-right flex">
-      <p id="top">Auto share careers through social media.<p>
+      <p>Auto share careers through social media.<p>
       <p id="bottom">Selected your profrssion? Great, create connection on this niche website.</p>
     </div>
   </div>
@@ -74,7 +72,7 @@
       <p id="box">3</p>
     </div>
     <div  id="3" class="list-right flex">
-      <p id="top">Free!</p>
+      <p>Free!</p>
       <p id="bottom">This site will offer free service to those who need to find work.</p>
     </div>
   </div>
@@ -131,7 +129,5 @@
     </div>
 
 </section>
-
-
 
 <?php get_footer(); ?>
